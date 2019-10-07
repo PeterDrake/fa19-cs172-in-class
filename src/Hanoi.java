65e8b81d71fbe75f8be441f030bed7800a077e9b
drake <drake@lclark.edu>
2019-10-07 14:49:13 -0700

    Tower of Hanoi.

---
+++ src/Hanoi.java
@@ -0,0 +1,18 @@
+public class Hanoi {
+
+    public static void main(String[] args) {
+        hanoi("a","b","c", 3);
+    }
+
+    static void hanoi(String start, String spare, String end, int n) {
+        if (n==1) {
+            StdOut.println(start+"-->"+end);
+        }
+        else {
+            hanoi(start,end,spare, n-1);
+            StdOut.println(start+"-->"+end);
+            hanoi(spare,start,end, n-1);
+        }
+
+    }
+}
