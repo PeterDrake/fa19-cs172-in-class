8854f39f6e8ee2fc035372e314c473002e5f9582
drake <drake@lclark.edu>
2019-11-18 13:41:42 -0800

    Queues and SetsAndMaps.

---
+++ src/SetsAndMaps.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class SetsAndMaps {
+
+    public static void main(String[] args) {
+        Map<Integer, String> numbers = new HashMap<>();
+        numbers.put(1, "one");
+        numbers.put(2, "two");
+        StdOut.println(numbers.get(2));
+//        Set<Integer> set = new HashSet<>();
+//        set.add(2);
+//        set.add(1);
+//        set.add(3);
+//        StdOut.println(set);
+    }
+
+}
